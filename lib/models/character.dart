22f482bc6046f672df0573a12a43ea1f85242a5c
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2024-05-23 19:18:59 -0400

    Added vocation enum  and skill class

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,13 +1,17 @@
+import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
+import 'package:flutter_rpg/models/vocation.dart';
 
 class Character with Stats {
 
   // constructor
   Character({
-    required this.name, required this.slogan, required this.id
+    required this.name, required this.slogan, required this.vocation, required this.id
   });
 
   // fields
+  final Set<Skill> skills = {};
+  final Vocation vocation;
   final String name;
   final String slogan;
   final String id;
@@ -19,5 +23,10 @@ class Character with Stats {
   // methods
   void toggleIsFav() {
     _isFav = !_isFav;
+  }
+
+  void updateSkill(Skill skill) {
+    skills.clear();
+    skills.add(skill);
   }
 }
